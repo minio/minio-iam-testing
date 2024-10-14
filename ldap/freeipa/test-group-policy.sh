5371cec0fa8b90646dbcbc5a2d3f12cae2f1ecb3
Allan Roger Reid <allanrogerreid@gmail.com>
2024-10-14 14:14:17 -0700

    Run shfmt (#2)

--- ldap/freeipa/test-group-policy.sh
+++ ldap/freeipa/test-group-policy.sh
@@ -13,33 +13,29 @@ ALIAS=test
 # set policy for managers group
 ${MC} admin policy set ${ALIAS} consoleAdmin group='cn=managers,cn=groups,cn=accounts,dc=demo1,dc=freeipa,dc=org'
 
-
-[ -f cookie.txt ]  && rm cookie.txt
+[ -f cookie.txt ] && rm cookie.txt
 
 curl -s 'http://localhost:9001/api/v1/login' \
-  --cookie-jar cookie.txt \
-  -H 'Content-Type: application/json' \
-  --data-raw '{"accessKey":"manager","secretKey":"Secret123"}' \
-  --output session-manager.json
+	--cookie-jar cookie.txt \
+	-H 'Content-Type: application/json' \
+	--data-raw '{"accessKey":"manager","secretKey":"Secret123"}' \
+	--output session-manager.json
 
-cat session-manager.json | grep 'invalid Login' &&  exit 1
+cat session-manager.json | grep 'invalid Login' && exit 1
 
 curl -s 'http://localhost:9001/api/v1/buckets' \
-  --cookie cookie.txt \
-  --output buckets-manager.json
+	--cookie cookie.txt \
+	--output buckets-manager.json
 
 cat buckets-manager.json | grep testbucket1 || exit 1
-
 
 # update readnly policy with "ListAllMyBuckets"
 
 curl -s 'http://localhost:9001/api/v1/policies' \
-  --cookie cookie.txt \
-  -H 'Content-Type: application/json' \
-  --data-raw '{"name":"readonly","policy":"{\n    \"Version\": \"2012-10-17\",\n    \"Statement\": [\n        {\n            \"Effect\": \"Allow\",\n            \"Action\": [\n                \"s3:GetBucketLocation\",\n                \"s3:GetObject\",\n                \"s3:ListBucket\"\n            ],\n            \"Resource\": [\n                \"arn:aws:s3:::*\"\n            ]\n        }\n    ]\n}"}' \
-  --output policies-update-manager.json
-
-
+	--cookie cookie.txt \
+	-H 'Content-Type: application/json' \
+	--data-raw '{"name":"readonly","policy":"{\n    \"Version\": \"2012-10-17\",\n    \"Statement\": [\n        {\n            \"Effect\": \"Allow\",\n            \"Action\": [\n                \"s3:GetBucketLocation\",\n                \"s3:GetObject\",\n                \"s3:ListBucket\"\n            ],\n            \"Resource\": [\n                \"arn:aws:s3:::*\"\n            ]\n        }\n    ]\n}"}' \
+	--output policies-update-manager.json
 
 cat policies-update-manager.json | grep ListBucket || exit 1
 
@@ -48,7 +44,6 @@ rm policies-update-manager.json
 rm session-manager.json
 rm buckets-manager.json
 echo "TEST manager OK"
-
 
 # 13136_minio1_1 | Error: expecting a policy to be set for user `uid=employee,cn=users,cn=accounts,dc=demo1,dc=freeipa,dc=org`
 # or one of their groups:
@@ -59,19 +54,19 @@ echo "TEST manager OK"
 # set policy for managers group
 ${MC} admin policy set ${ALIAS} readonly group='cn=employees,cn=groups,cn=accounts,dc=demo1,dc=freeipa,dc=org'
 
-[ -f cookie.txt ]  && rm cookie.txt
+[ -f cookie.txt ] && rm cookie.txt
 
 curl -s 'http://localhost:9001/api/v1/login' \
-  --cookie-jar cookie.txt \
-  -H 'Content-Type: application/json' \
-  --data-raw '{"accessKey":"employee","secretKey":"Secret123"}' \
-  --output session-employee.json
+	--cookie-jar cookie.txt \
+	-H 'Content-Type: application/json' \
+	--data-raw '{"accessKey":"employee","secretKey":"Secret123"}' \
+	--output session-employee.json
 
-cat session-employee.json | grep 'invalid Login' &&  exit 1
+cat session-employee.json | grep 'invalid Login' && exit 1
 
 curl -s 'http://localhost:9001/api/v1/buckets' \
-  --cookie cookie.txt \
-  --output buckets-employee.json
+	--cookie cookie.txt \
+	--output buckets-employee.json
 
 cat buckets-employee.json | grep null && exit 1
 
--- ldap/freeipa/test-init.sh
+++ ldap/freeipa/test-init.sh
@@ -1,4 +1,3 @@
-
 MCC=$(docker-compose ps | grep _mc_1 | awk '{print $1}')
 DEXEC="docker exec -ti ${MCC}"
 MC="${DEXEC} mc"
@@ -13,14 +12,14 @@ ${MC} ls ${ALIAS}
 # config identity_ldap to ipa.demo1.freeipa.org
 # see https://www.freeipa.org/page/Demo
 ${MC} admin config set ${ALIAS} \
-identity_ldap \
-server_addr="ipa.demo1.freeipa.org:636" \
-tls_skip_verify=on \
-lookup_bind_dn="uid=admin,cn=users,cn=accounts,dc=demo1,dc=freeipa,dc=org" \
-lookup_bind_password="Secret123" \
-user_dn_search_base_dn="cn=users,cn=accounts,dc=demo1,dc=freeipa,dc=org" \
-user_dn_search_filter="(&(uid=%s))" \
-group_search_base_dn="cn=groups,cn=accounts,dc=demo1,dc=freeipa,dc=org" \
-group_search_filter="(&(objectClass=groupofnames)(objectClass=nestedgroup)(member=%d))"
+	identity_ldap \
+	server_addr="ipa.demo1.freeipa.org:636" \
+	tls_skip_verify=on \
+	lookup_bind_dn="uid=admin,cn=users,cn=accounts,dc=demo1,dc=freeipa,dc=org" \
+	lookup_bind_password="Secret123" \
+	user_dn_search_base_dn="cn=users,cn=accounts,dc=demo1,dc=freeipa,dc=org" \
+	user_dn_search_filter="(&(uid=%s))" \
+	group_search_base_dn="cn=groups,cn=accounts,dc=demo1,dc=freeipa,dc=org" \
+	group_search_filter="(&(objectClass=groupofnames)(objectClass=nestedgroup)(member=%d))"
 
 ${MC} admin service restart ${ALIAS}
