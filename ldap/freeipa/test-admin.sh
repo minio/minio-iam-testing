a1e043ca31f971f0e40464df49d68eb2cea79062
Allan Reid <allanrogerreid@gmail.com>
2024-10-14 14:12:19 -0700

    Run shfmt

--- ldap/freeipa/test-admin.sh
+++ ldap/freeipa/test-admin.sh
@@ -7,19 +7,19 @@ ALIAS=test
 ${MC} admin policy set ${ALIAS} consoleAdmin user='uid=admin,cn=users,cn=accounts,dc=demo1,dc=freeipa,dc=org'
 ${MC} admin user list ${ALIAS}
 
-[ -f cookie.txt ]  && rm cookie.txt
+[ -f cookie.txt ] && rm cookie.txt
 
 curl -s 'http://localhost:9001/api/v1/login' \
-  --cookie-jar cookie.txt \
-  -H 'Content-Type: application/json' \
-  --data-raw '{"accessKey":"admin","secretKey":"Secret123"}' \
-  --output session-admin.json
+	--cookie-jar cookie.txt \
+	-H 'Content-Type: application/json' \
+	--data-raw '{"accessKey":"admin","secretKey":"Secret123"}' \
+	--output session-admin.json
 
-cat session-admin.json | grep 'invalid Login' &&  exit 1
+cat session-admin.json | grep 'invalid Login' && exit 1
 
 curl -s 'http://localhost:9001/api/v1/buckets' \
-  --cookie cookie.txt \
-  --output buckets-admin.json
+	--cookie cookie.txt \
+	--output buckets-admin.json
 
 cat buckets-admin.json | grep testbucket1 || exit 1
 
